03bcb53f1a37ee6b812b4736ee2794fda2c6d051
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-10-19 08:26:15 +0100

    add filter for routes require auth

---
+++ src/main/java/com/optima/resourcium_optima/filters/AuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.optima.resourcium_optima.filters;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthenticationFilter", urlPatterns = {"/dashboard"})
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        // Check if the user is authenticated (you can use a session attribute, cookies, etc.)
+        if (httpRequest.getSession().getAttribute("user") == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
